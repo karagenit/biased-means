c69fc4ecc5c659745744399f6ac35041112a3a52
Caleb Smith <karagenit@outlook.com>
2018-02-28 10:43:04 -0500

    Add Biased Mean Order Calculations

--- script.rb
+++ script.rb
@@ -25,13 +25,29 @@ true_pooled[:sd]   = true_individuals.map{ |ind| ind[:mean] }.standard_deviation
 
 true_order = true_individuals.sort_by { |ind| ind[:mean] }
 
+######## BEGIN SAMPLE CALCULATIONS ########
+
 sample_individuals = []
 
 (0...INDIVIDUALS).each do |i|
   set = gaussian_set(true_individuals[i][:mean], true_individuals[i][:sd], Random.rand * 90 + 10)
-  sample_individuals[i] = { index: i, mean: set.mean, sd: set.standard_deviation }
+  sample_individuals[i] = { index: i, mean: set.mean, sd: set.standard_deviation, n: set.length }
 end
+
+sample_pooled = {}
+sample_pooled[:mean] = sample_individuals.map{ |ind| ind[:mean] }.mean
+sample_pooled[:sd]   = sample_individuals.map{ |ind| ind[:mean] }.standard_deviation
+sample_pooled[:n]    = sample_individuals.inject(0){ |sum, ind| sum + ind[:n] }
 
 mean_rank_order = sample_individuals.sort_by { |ind| ind[:mean] }
 
 puts misplacements(true_order, mean_rank_order)
+
+biased_mean_order = sample_individuals.sort_by do |ind|
+  sample_dev = ind[:sd] / Math.sqrt(ind[:n])
+  pooled_dev = sample_pooled[:sd] / Math.sqrt(sample_pooled[:n])
+
+  (ind[:mean] * pooled_dev/sample_dev) + (sample_pooled[:mean] * sample_dev/pooled_dev)
+end
+
+puts misplacements(true_order, biased_mean_order)
