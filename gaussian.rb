3f90db2503a6d69b1ee410d95e4a6e624aab9d59
Caleb Smith <karagenit@outlook.com>
2018-02-28 08:39:51 -0500

    Add Binning Method

--- gaussian.rb
+++ gaussian.rb
@@ -24,13 +24,17 @@ def gaussian_set(mean, sd, size)
   nums
 end
 
-nums = gaussian_set(0, 1, 100)
-data = {}
-
-nums.each do |num|
-  val = floor_float(num, 0.5)
-  data[val] = 0 if data[val].nil?
-  data[val] += 1
+def bin_nums(nums, binsize)
+  data = {}
+  nums.each do |num|
+    val = floor_float(num, binsize)
+    data[val] = 0 if data[val].nil?
+    data[val] += 1
+  end
+  data
 end
+
+nums = gaussian_set(0, 1, 1000)
+data = bin_nums(nums, 0.5)
 
 p data.sort.to_h
