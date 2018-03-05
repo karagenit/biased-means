8692783aedf519776a5f094f89d1474bc9b20501
Caleb Smith <karagenit@outlook.com>
2018-03-04 20:36:59 -0500

    Add Trials & Progress Output

--- script.rb
+++ script.rb
@@ -27,33 +27,42 @@ true_order = true_individuals.sort_by { |ind| ind[:mean] }
 
 ######## BEGIN SAMPLE CALCULATIONS ########
 
-sample_individuals = []
+mean_rank_misplacements = []
+biased_mean_misplacements = []
 
-(0...INDIVIDUALS).each do |i|
-  set = gaussian_set(true_individuals[i][:mean], true_individuals[i][:sd], Random.rand * 90 + 10)
-  sample_individuals[i] = { index: i, mean: set.mean, sd: set.standard_deviation, n: set.length }
+(0..1000).each do |trial|
+
+  print "Progress: #{ (100 * trial / 1000).round }%     \r"
+
+  sample_individuals = []
+
+  (0...INDIVIDUALS).each do |i|
+    set = gaussian_set(true_individuals[i][:mean], true_individuals[i][:sd], Random.rand * 90 + 10)
+    sample_individuals[i] = { index: i, mean: set.mean, sd: set.standard_deviation, n: set.length }
+  end
+
+  sample_pooled = {}
+  sample_pooled[:mean] = sample_individuals.map{ |ind| ind[:mean] }.mean
+  sample_pooled[:sd]   = sample_individuals.map{ |ind| ind[:mean] }.standard_deviation
+  sample_pooled[:n]    = sample_individuals.inject(0){ |sum, ind| sum + ind[:n] }
+
+  mean_rank_order = sample_individuals.sort_by { |ind| ind[:mean] }
+
+  mean_rank_misplacements.push misplacements(true_order, mean_rank_order)
+
+  biased_mean_order = sample_individuals.sort_by do |ind|
+    #sample_dev = ind[:sd] / Math.sqrt(ind[:n])
+    #pooled_dev = sample_pooled[:sd] / Math.sqrt(sample_pooled[:n])
+    #ratio = pooled_dev/sample_dev
+
+    ratio = ind[:sd] / sample_pooled[:sd]
+
+    biased_mean = (ind[:mean] * (1-ratio)) + (sample_pooled[:mean] * (ratio))
+  end
+
+  biased_mean_misplacements.push misplacements(true_order, biased_mean_order)
 end
 
-sample_pooled = {}
-sample_pooled[:mean] = sample_individuals.map{ |ind| ind[:mean] }.mean
-sample_pooled[:sd]   = sample_individuals.map{ |ind| ind[:mean] }.standard_deviation
-sample_pooled[:n]    = sample_individuals.inject(0){ |sum, ind| sum + ind[:n] }
-
-puts "Pooled Mean: #{sample_pooled[:mean]}"
-
-mean_rank_order = sample_individuals.sort_by { |ind| ind[:mean] }
-
-puts misplacements(true_order, mean_rank_order)
-
-biased_mean_order = sample_individuals.sort_by do |ind|
-  #sample_dev = ind[:sd] / Math.sqrt(ind[:n])
-  #pooled_dev = sample_pooled[:sd] / Math.sqrt(sample_pooled[:n])
-  #ratio = pooled_dev/sample_dev
-
-  ratio = ind[:sd] / sample_pooled[:sd]
-
-  biased_mean = (ind[:mean] * (1-ratio)) + (sample_pooled[:mean] * (ratio))
-  puts "Ratio: #{ratio.round(2)}\tMean: #{ind[:mean].round(2)}\tBiased Mean: #{biased_mean.round(2)}"
-end
-
-puts misplacements(true_order, biased_mean_order)
+puts ""
+puts mean_rank_misplacements.mean
+puts biased_mean_misplacements.mean
